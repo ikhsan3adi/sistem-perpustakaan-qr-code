5754f8a8fae0003e25a913c59e2cd7149d6197e4
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-08-13 14:19:13 +0700

    deps: add codeigniter shield

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+service('auth')->routes($routes);
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -48,6 +48,8 @@ abstract class BaseController extends Controller
      */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
+        $this->helpers = array_merge($this->helpers, ['setting']);
+
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
