db3c21bb00a9e07a00e852a5ddb7743273c8b127
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-08-16 20:15:31 +0700

    Change app timezone

--- app/Config/App.php
+++ app/Config/App.php
@@ -110,7 +110,7 @@ class App extends BaseConfig
      *
      * @see https://www.php.net/manual/en/timezones.php for list of timezones supported by PHP.
      */
-    public string $appTimezone = 'UTC';
+    public string $appTimezone = 'Asia/Jakarta';
 
     /**
      * --------------------------------------------------------------------------
